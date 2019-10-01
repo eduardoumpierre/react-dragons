dfa04d8ecb01559044c949dff83c26b9bd7ccb18
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2019-10-01 09:15:08 -0300

    :sparkles: :recycle: get dragon service added

--- src/services/dragons.js
+++ src/services/dragons.js
@@ -1,8 +1,7 @@
 import Axios from 'axios';
 import { DRAGON_API } from '../config/constants';
 
-const http = Axios.create({
-  baseURL: DRAGON_API,
-});
+const axios = Axios.create({ baseURL: DRAGON_API });
 
-export const getDragonList = () => http.get();
+export const getDragon = id => axios.get(`/${id}`);
+export const getDragonList = () => axios.get();
