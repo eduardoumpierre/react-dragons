43fd8fad535a8cf02991eea2d6b2943af00daa60
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2019-10-01 09:16:03 -0300

    :sparkles: redux devtools extension added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import dragons from './dragons/reducer';
 
 export default createStore(
   combineReducers({ dragons }),
-  applyMiddleware(thunkMiddleware)
+  composeWithDevTools(applyMiddleware(thunkMiddleware))
 );
