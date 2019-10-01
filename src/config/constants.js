4278f186ac67184c0ae97b6c86bc48caf3fb0c06
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2019-10-01 09:03:41 -0300

    :recycle: :sparkles: getRoute function created

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,11 @@
 export const ROUTES = {
-  LIST: '/',
+  HOME: '/',
   FORM: '/form/:id?',
   DETAIL: '/detail/:id',
 };
+
+export const getRoute = (route, params) =>
+  route.replace(/(:\w+)/g, match => params[match.substr(1)]);
 
 export const DRAGON_API =
   'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon';
